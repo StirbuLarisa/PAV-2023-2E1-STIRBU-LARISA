5c1fd899389c1c5b532a73caf8e2740ffcb87707
Stirbu.Larisa <eusuntlarisa@gmail.com>
2023-03-07 21:37:18 +0200

    tema

--- Homework1/src/Main.java
+++ Homework1/src/Main.java
@@ -11,54 +11,44 @@ public class Main {
             System.out.println("This is not a number");
             return;
         }
-        long start = System.nanoTime();
-        String[][] matrix = new String[n][n];
+        int[][] matrix = new int[n][n];
         int i, j;
 
-        try{
-            for (i = 0; i < n; i++) {
-                for (j = 0; j < n; j++) {
-                    int num = (i + j) % n + 1;
-                    matrix[i][j] = Integer.toString(num);
-                }
+        long start = System.nanoTime();
+
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n; j++) {
+                matrix[i][j] = (i + j) % n + 1;
             }
-        }catch (Exception exp){
-            long end = System.nanoTime();
-            long time = end-start;
+        }
+
+        long end = System.nanoTime();
+        long time = end - start;
+
+        if (n > 30000) {
             System.out.println(time);
-            return;
-        }
-        for (i = 0; i < n; i++) {
-            for (j = 0; j < n; j++) {
-                System.out.print(matrix[i][j] + " ");
+        } else {
+            String matrix1 = "";
+            String matrix2 = "";;
+            System.out.println("Rows:");
+
+            for (i = 0; i < n; i++) {
+                String col = "";
+                String row = "";
+
+                for (j = 0; j < n; j++) {
+                    row = row.concat(Integer.toString(matrix[i][j]));
+                    col = col.concat(Integer.toString(matrix[j][i]));
+                }
+                matrix1 = matrix1.concat(row + "\n");
+                matrix2 = matrix2.concat(col + "\n");
+
             }
-            System.out.println();
+            System.out.println(matrix1);
+            System.out.println(matrix2);
+
         }
-
-        System.out.println("Rows:");
-
-        for (i = 0; i < n; i++) {
-            String string = "";
-            for (j = 0; j < n; j++) {
-                string = string.concat(matrix[i][j]);
-            }
-            System.out.println(string);
-        }
-
-        System.out.println("Columns:");
-
-        for (i = 0; i < n; i++) {
-            String string = "";
-            for (j = 0; j < n; j++) {
-                string = string.concat(matrix[j][i]);
-            }
-            System.out.println(string);
-        }
-
-
 
 
     }
-
-
 }
